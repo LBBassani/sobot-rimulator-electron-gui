15bb3a36f977d04cf46a272b4a835785afc74ebd
LBBassani <lorenabassani12@gmail.com>
2020-06-04 16:02:41 -0300

    Desenha retangulos :D

--- renderer.js
+++ renderer.js
@@ -12,3 +12,16 @@ const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
 }
+
+// Desenhando no canva
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "red";
+ctx.fillRect(20, 20, 75, 50);
+//Turn transparency on
+ctx.globalAlpha = 0.2;
+ctx.fillStyle = "blue";
+ctx.fillRect(50, 50, 75, 50);
+ctx.fillStyle = "green";
+ctx.fillRect(80, 80, 75, 50);
