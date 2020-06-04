2ea83032d8fb5f01c082a8b49cf7163a631c5825
LBBassani <lorenabassani12@gmail.com>
2020-06-04 17:35:20 -0300

    funções de desenho
    stroke: circulo, retangulo e linha
    draw: circulo e retangulo

--- renderer.js
+++ renderer.js
@@ -5,7 +5,7 @@ pynode.appendSysPath('./pysrimulator/')
 pynode.openFile('api')
 pynode.call('echo', "Eu chamei uma função Python!!!!!", (err, result) => {
     if (err) return console.log('error : ', err)
-    else return replaceText('text-canvas',result) // true
+    else return replaceText('img-canvas',result) // true
   })
 
 const replaceText = (selector, text) => {
@@ -13,7 +13,7 @@ const replaceText = (selector, text) => {
     var ctx = element.getContext("2d")
     ctx.fillStyle = "red";
     ctx.textAlign = "center";
-    ctx.fillText(text, element.width/2, element.height/2);
+    ctx.fillText(text, 75, 10);
 }
 
 // Desenhando no canva
@@ -21,16 +21,62 @@ const replaceText = (selector, text) => {
 var c = document.getElementById("img-canvas");
 var ctx = c.getContext("2d");
 ctx.translate(50, 0);
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 75, 50);
+
+
+for (let i = 1; i < 6; i++) {
+  for (let j = 0; j < 6; j++) {
+    strokeStyle = `rgb(
+        0,
+        ${Math.floor(255 - 42.5 * i)},
+        ${Math.floor(255 - 42.5 * j)})`;
+    stroke_circle(ctx, 12.5 + j * 25, 12.5 + i * 25, 10, strokeStyle, 1, 1 );
+  }
+}
+
 //Turn transparency on
-ctx.globalAlpha = 0.2;
-ctx.fillStyle = "blue";
-ctx.fillRect(30, 30, 75, 50);
-ctx.fillStyle = "green";
-ctx.fillRect(60, 60, 75, 50);
-ctx.fillStyle = "purple";
-ctx.globalAlpha = 0.5;
-ctx.arc(70, 55, 50, 0, 2 * Math.PI);
-ctx.fill();
-ctx.fillStyle = "black";
+function draw_rect(context, x, y, l, h, color, alpha){
+  context.beginPath();
+  context.globalAlpha = alpha;
+  context.fillStyle = color;
+  context.fillRect(x, y, l, h);
+  context.closePath();
+}
+
+function stroke_rect(context, x, y, l, h, color, lineWidth, alpha){
+  context.beginPath();
+  context.globalAlpha = alpha;
+  context.lineWidth = lineWidth;
+  context.strokeStyle = color;
+  context.strokeRect(x, y, l, h);
+  context.closePath();
+}
+
+function draw_circle(context, xc, yc, radius, color, alpha){
+  context.beginPath();
+  context.fillStyle = color;
+  context.globalAlpha = alpha;
+  context.arc(xc, yc, radius, 0, 2 * Math.PI);
+  context.fill();
+  context.closePath();
+}
+
+function stroke_circle(context, xc, yc, radius, color, lineWidth, alpha){
+  context.beginPath();
+  context.strokeStyle = color;
+  context.globalAlpha = alpha;
+  context.lineWidth = lineWidth;
+  context.arc(xc, yc, radius, 0, 2 * Math.PI);
+  context.stroke();
+  context.closePath();
+}
+
+function stroke_line(context, xi, yi, xf, yf, color, lineWidth , alpha){
+  context.beginPath();
+  context.globalAlpha = alpha;
+  context.strokeStyle = color;
+  context.lineWidth = lineWidth;
+  context.moveTo(xi, yi);
+  context.lineTo(xf, yf);
+  context.stroke();
+  context.closePath();
+}
