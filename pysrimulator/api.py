4f123f3854f58a03410a1ce23a3401bada56b961
LBBassani <lorenabassani12@gmail.com>
2020-06-04 23:07:42 -0300

    Python envia frame :D

--- pysrimulator/api.py
+++ pysrimulator/api.py
@@ -1,5 +1,26 @@
 # Python code
 
 def echo(text):
-        """echo any text"""
-        return text
+    """echo any text"""
+    return text
+
+from srimulatorcore.rimulators_samples.rimulator import Rimulator
+from srimulatorcore.gui.viewer import Viewer
+from srimulatorcore.sobots_samples.kheperaiii.kheperaiii import Kheperaiii
+
+rimulator = None
+
+def new_rimulator():
+    global rimulator
+    rimulator = Rimulator([(Kheperaiii, [1.0, 1.0])])
+    rimulator.add_viewer(Viewer(rimulator))
+    rimulator.initialize_sim(True)
+    return True
+
+
+def get_frame():
+    global rimulator
+    rimulator.step_sim_once()
+    rimulator.draw_world()
+    return rimulator.viewer.current_frame.draw_list
+
--- renderer.js
+++ renderer.js
@@ -8,27 +8,24 @@ pynode.appendSysPath('./pysrimulator/')
 pynode.openFile('api')
 
 // Chama função python echo para testar a integração python-node via PyNode
-pynode.call('echo', "Eu chamei uma função Python!!!!!", (err, result) => {
+pynode.call('echo', "Desenhando frame de simulação", (err, result) => {
     if (err) return console.log('error : ', err)
-    else return replaceText('img-canvas',result) // true
-  })
+    else return replaceText('title-pyteste', result) // true
+})
 
 // Função que substitui o texto na tela
 const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
-    var ctx = element.getContext("2d")
-    ctx.fillStyle = "red";
-    ctx.textAlign = "center";
-    ctx.fillText(text, 75, 10);
+    if (element) element.innerText = text
 }
 
 // Desenhando no canvas
 var c = document.getElementById("img-canvas");
 var ctx = c.getContext("2d");
-ctx.translate(50, 0);
+//ctx.translate(50, 0);
 
 // Desenha diversos circulos com gradiente
-for (let i = 1; i < 6; i++) {
+/* for (let i = 1; i < 6; i++) {
   for (let j = 0; j < 6; j++) {
     strokeStyle = `rgb(
         0,
@@ -36,4 +33,28 @@ for (let i = 1; i < 6; i++) {
         ${Math.floor(255 - 42.5 * j)})`;
     illustrator.stroke_circle(ctx, 12.5 + j * 25, 12.5 + i * 25, 10, strokeStyle, 1, 1 );
   }
+} */
+
+var rimulator = false;
+
+function new_frame(context){
+  console.log(rimulator)
+  if (rimulator) {
+    frame = pynode.call("get_frame", (err, result) =>{
+      if(err) return console.error(err);
+      else return console.log(result);
+    });
+  }else{
+    pynode.call("new_rimulator", (err, result)=>{
+      if (err) return console.error(err);
+      else return rimulator = result;
+    } );
+  }
+}
+
+botao = document.getElementById("button-pyteste");
+botao.addEventListener("click", new_frame);
+
+function draw_frame(frame){
+  // TODO: Deixar para 05/06/2020
 }
